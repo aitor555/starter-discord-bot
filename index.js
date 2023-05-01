27eddad6920666e43f6fe2a6bf9de6e59108f656
aitor555 <herr.tiss94@gmail.com>
2023-05-01 19:18:45 +0200

    fixed ENV stuff

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
-const { token } = require('./config.json');
+const token = process.env.TOKEN;
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
